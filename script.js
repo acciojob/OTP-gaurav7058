a017385fcadd457b1a3c9dcffb5466b38bc0c314
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-08-01 12:12:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,48 +1,15 @@
-let codes = document.querySelectorAll(".code")
+const codes = document.querySelectorAll('.code');
 
-// change, input
+        codes.forEach((code, idx) => {
+            code.addEventListener('input', () => {
+                if (code.value.length === 1 && idx < codes.length - 1) {
+                    codes[idx + 1].focus();
+                }
+            });
 
-codes.forEach((code,index)=>{
-    code.addEventListener("input",()=>{
-
-        if(code.value.length == 1){
-
-             if(index < codes.length-1){
-
-                   codes[index+1].focus()
-
-                   codes[index+1].classList.add("focused")
-
-             }
-
-        }
-
-    })
-
-    // keyup, keydown(press)
-
-    code.addEventListener("keydown",(e)=>{
-
-        if(e.key == "Backspace"){
-
-              if(index > 0 && code.value.length == 0){
-
-                
-
-                codes[index-1].focus()
-
-                codes[index-1].classList.add("focused")
-
-              }
-
-        }
-
-    })
-
-    code.addEventListener("blur", ()=>{
-
-          code.classList.remove("focused")
-
-    })
-
-})
+            code.addEventListener('keydown', (e) => {
+                if (e.key === 'Backspace' && code.value.length === 0 && idx > 0) {
+                    codes[idx - 1].focus();
+                }
+            });
+        });
